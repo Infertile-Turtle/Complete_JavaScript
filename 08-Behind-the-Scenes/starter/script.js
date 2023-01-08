0abf36deae8a595c0459194a8508f7ffcc646d95
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2023-01-08 11:12:17 -0600

    push

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,3 +1,16 @@
 'use strict';
-//test
-//test
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  console.log(firstName);
+  return age;
+}
+
+const firstName = `Andrew`;
+console.log(calcAge(1993));
+// console.log(age);
